5c76a561bb2050879ad1de4f6a4946cbd1bc8a78
pablo <4d.garcia6@gmail.com>
2015-07-28 21:05:58 +0200

    Also login with user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,22 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
+  validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }, uniqueness: true
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  # Virtual attribute for authenticating by either username or email
+  # This is an addition to a real persisted field, 'user_name'
+  attr_accessor :login
+
+  def self.find_for_database_authentication warden_conditions
+  	conditions = warden_conditions.dup
+  	if login = conditions.delete(:login)
+  		where(conditions.to_hash).where(["user_name = :value OR lower(email) = lower(:value)", { value: login }]).first
+  	else
+  		conditions[:email].downcase! if conditions[:email]
+  		where(conditions.to_hash).first
+  	end
+  end
 end
