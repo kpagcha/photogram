b99043e83a88a6b51dab3a095b4ccdcba24a995f
pablo <4d.garcia6@gmail.com>
2015-07-28 16:45:43 +0200

    Destroy Post method added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	before_action :find_post, only: [ :show, :edit, :update ]
+	before_action :find_post, only: [ :show, :edit, :update, :destroy ]
 
 	def index
 		@posts = Post.order "created_at DESC"
@@ -31,6 +31,11 @@ class PostsController < ApplicationController
 		else
 			render 'edit'
 		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
 	end
 
 	private
